8d01e35f399099106c594e3824af872dd7fa769d
nontachai <pappaliving6395@gmail.com>
2025-02-23 20:16:08 +0700

    update mode-toggle and profile pic

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -6,33 +6,27 @@ import { useTheme } from "next-themes"
 
 
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme()
+  const { resolvedTheme, setTheme } = useTheme()
   const [isHydrated, setIsHydrated] = React.useState(false);
 
   React.useEffect(() => {
     setIsHydrated(true)
   }, []);
 
-
   return (
     <>
-      {isHydrated && theme === "dark" ? (
-      <button 
-          className="flex flex-col bg-slate-900
-          group items-center justify-center rounded-[24px] 
-          w-14 h-14 hover:text-lime-500 duration-300" 
-          onClick={() => setTheme("light")}>
-          <Sun className="w-5 h-5 flex-shrink-0 " />
-        </button>
-      ) : (
-        <button 
-          className="flex flex-col bg-slate-50
-          group items-center justify-center rounded-[24px] 
-          w-14 h-14 hover:text-lime-500 duration-300"   
-          onClick={() => setTheme("dark")}>
-        <Moon className="w-5 h-5 flex-shrink-0" />
+      <button
+        className={`flex flex-col ${
+          resolvedTheme === "dark" ? "bg-slate-800" : "bg-slate-50"
+        } group items-center justify-center rounded-[24px] w-14 h-14 hover:text-teal-500 duration-300`}
+        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      >
+        {isHydrated && resolvedTheme === "dark" ? (
+          <Sun className="w-5 h-5 flex-shrink-0" />
+        ) : (
+          <Moon className="w-5 h-5 flex-shrink-0" />
+        )}
       </button>
-      )}
     </>
     
   )
